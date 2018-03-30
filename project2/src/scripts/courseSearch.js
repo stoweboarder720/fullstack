f7053d44dfe766cf049fdefc0923299fbb6da1e1
Christian L. Ouellette <clo006@bucknell.edu>
2018-03-30 11:16:19 -0400

    new build

--- project2/src/scripts/courseSearch.js
+++ project2/src/scripts/courseSearch.js
@@ -3,14 +3,14 @@ import Refs from "../data/refs.js";
 
 class CourseSearch extends React.Component {
   courseQueryFetch(query) {
-    return fetch(Refs.SERVER + "q?" + query + "&Semester=Fall&Year=2018&limit=1000")
+    return fetch(Refs.SERVER + "q?" + query + "&Semester=Fall&Year=2018&limit=500")
       .then((response) => response.json());
   }
 
   crnDeptFetch(courseID) {
     const department = courseID.slice(0, 4).toUpperCase();
     const crn = courseID.substring(4).trim().toUpperCase();
-    return fetch(Refs.SERVER + `q?Department=${department}&CrseNum=${crn}&Semester=Fall&Year=2018&limit=100`)
+    return fetch(Refs.SERVER + `q?Department=${department}&CrseNum=${crn}&Semester=Fall&Year=2018&limit=500`)
       .then((response) => response.json());
   }
 
